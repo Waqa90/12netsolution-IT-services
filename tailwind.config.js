43ab9444c171a60ce3114b46185158c3794febda
Waqa90 <74024085+Waqa90@users.noreply.github.com>
2026-07-16 11:15:40 +1200

    Extend Tailwind theme with custom brand colors
    Added custom brand colors to the Tailwind CSS theme.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+          extend: {
+                  colors: {
+                            brand: {
+                                        50: '#f5f2ed',
+                                        100: '#e8e0d3',
+                                        200: '#d2c0a3',
+                                        300: '#b89f76',
+                                        400: '#8f7350',
+                                        500: '#6b5439',
+                                        600: '#4a3826',
+                                        700: '#3a2b1d',
+                                        800: '#2c2015',
+                                        900: '#1e1610',
+                            },
+                  },
+          },
+    },
+    plugins: [],
 };
